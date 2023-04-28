3d7bfdcdae48e23936be64dcd879f6646fb80f52
kedjzxc <hailrake27@gmail.com>
2023-04-28 12:52:57 +0300

    feat: add logic to specific btns

--- js/script.js
+++ js/script.js
@@ -14,6 +14,13 @@ const arrowUp = document.querySelector('.keyboard__btn-up')
 const arrowDown = document.querySelector('.keyboard__btn-down')
 const arrowLeft = document.querySelector('.keyboard__btn-left')
 const arrowRight = document.querySelector('.keyboard__btn-right')
+const specBtns = [space, backspace, tab, del, caps, leftShift, rigthShift, ctrlLeft, ctrlRight, enter, arrowDown, arrowLeft, arrowUp, arrowRight]
+
+specBtns.forEach(btn => {
+  btn.addEventListener('mousedown', () => {
+    btn.classList.add('active')
+  })
+})
 
 window.addEventListener('keydown', (e) => {
   if (e.key == 'Tab') {
@@ -78,7 +85,7 @@ window.addEventListener('keydown', (e) => {
       arrowRight.classList.add('active')
     }
   })
-  console.log(e);
+  // console.log(e);
 })
 
 window.addEventListener('keyup', (e) => {
@@ -130,20 +137,89 @@ window.addEventListener('keyup', (e) => {
     }
   })
 })
-btns.forEach(item => {
-  item.addEventListener('click', () => {
-    input.value += item.innerHTML
+// btns.forEach(item => {
+//   item.addEventListener('click', () => {
+//     input.value += item.innerHTML
+//   })
+// })
+
+for (let i = 0; i < btns.length; i++) {
+  if (btns[i].classList.contains('keyboard__btn-enter')) continue
+  if (btns[i].classList.contains('keyboard__btn-backspace')) continue
+  if (btns[i].classList.contains('keyboard__btn-tab')) continue
+  if (btns[i].classList.contains('keyboard__btn-space')) continue
+  if (btns[i].classList.contains('keyboard__btn-del')) continue
+  if (btns[i].classList.contains('keyboard__btn-shift--right')) continue
+  if (btns[i].classList.contains('keyboard__btn-shift--left')) continue
+  if (btns[i].classList.contains('keyboard__btn-win')) continue
+  if (btns[i].classList.contains('keyboard__btn-alt')) continue
+  if (btns[i].classList.contains('keyboard__btn-ctrl--right')) continue
+  if (btns[i].classList.contains('keyboard__btn-ctrl--left')) continue
+  if (btns[i].classList.contains('keyboard__btn-left')) continue
+  if (btns[i].classList.contains('keyboard__btn-up')) continue
+  if (btns[i].classList.contains('keyboard__btn-down')) continue
+  if (btns[i].classList.contains('keyboard__btn-right')) continue
+  if (btns[i].classList.contains('keyboard__btn-caps')) continue
+  btns[i].addEventListener('mousedown', () => {
+    input.value += btns[i].innerHTML.toLowerCase()
+    btns[i].classList.add('active')
   })
+}
+
+for (let i = 0; i < btns.length; i++) {
+  if (btns[i].classList.contains('keyboard__btn-caps')) continue
+  btns[i].addEventListener('mouseup', () => {
+    btns[i].classList.remove('active')
+  })
+}
+
+for (let i = 0; i < btns.length; i++) {
+  if (btns[i].classList.contains('active')) {
+    btns[i].addEventListener('click', () => {
+      input.value += btns[i].innerHTML.toUpperCase()
+      // btns[i].classList.add('active')
+    })
+  }
+ }
+
+backspace.addEventListener('click', () => {
+  const currentPosition = input.selectionStart;
+  const inputValue = input.value;
+  const newValue = inputValue.substr(0, currentPosition - 1) + inputValue.substr(currentPosition);
+  input.value = newValue;
+  input.selectionStart = currentPosition - 1;
+  input.selectionEnd = currentPosition - 1;
 })
 
-btns.forEach(item => {
-  item.addEventListener('mousedown', () => {
-    item.classList.add('active')
-  })
+enter.addEventListener('click', () => {
+  input.value += "\n"
 })
 
-btns.forEach(item => {
-  item.addEventListener('mouseup', () => {
-    item.classList.remove('active')
-  })
+space.addEventListener('click', () => {
+  input.value += ' '
 })
+
+del.addEventListener('click', () => {
+  const currentPosition = input.selectionStart;
+  const inputValue = input.value;
+  const newValue = inputValue.substr(0, currentPosition) + inputValue.substr(currentPosition + 1);
+  input.value = newValue;
+  input.selectionStart = currentPosition;
+  input.selectionEnd = currentPosition;
+})
+
+arrowLeft.addEventListener('click', () => {
+  const newPosition = input.selectionStart - 1
+  input.setSelectionRange(newPosition)
+})
+
+arrowRight.addEventListener('click', () => {
+  const newPosition = input.selectionStart + 1
+  input.setSelectionRange(newPosition)
+})
+
+
+// TODO: доелать логику для капса
+// сделать англ раскладку
+// сделать так, чтобы клавиатура генерировалась из js кода и убрать ее из html файла
+// опционально: чуть подправить саму клавиатуру (убрать лишние буквы, добавить нужные)
